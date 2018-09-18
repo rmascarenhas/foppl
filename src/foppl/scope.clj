7e6b0069b314a284c591830fbb77b7f7c25a0a5b
Renato Costa <renatomc@cs.ubc.ca>
2018-09-17 20:14:07 -0700

    Better documentation.

--- src/foppl/scope.clj
+++ src/foppl/scope.clj
@@ -1,8 +1,14 @@
 (ns foppl.scope
   "Validates the scope of variables used in a FOPPL program.
-  Traverses the AST but never performs any mutation."
+  Ensures that every variable used can be resolved (was previously
+  bound) as well as every function application is resolved to a
+  known function. Traverses the AST but never performs any mutation."
   (:require [foppl.ast :as ast :refer [accept]])
   (:require [foppl.utils :as utils]))
+
+;; ================================================================ ;;
+;;                             BUILT-INS                            ;;
+;; ================================================================ ;;
 
 (def ^:private builtin-functions
   ['+
@@ -27,26 +33,46 @@
    'bernoulli
    'flip])
 
+;; built-ins provided by FOPPL: some functions inherited from the host
+;; (Clojure), sample/observe, as well as a slate of distribution
+;; functions
 (def ^:private builtins
   (set (concat builtin-functions distributions)))
 
+;; ================================================================ ;;
+;;                          ENVIRONMENTS                            ;;
+;; ================================================================ ;;
+
+;; the environment of an expression determines whether or not variables
+;; are free or bound. Environments can be nested: nesting happens when
+;; performing local bind ('let' expressions)
 (defrecord environment [parent names])
 
 (defn- new-environment []
+  "Creates an empty, root environment"
   (environment. nil #{}))
 
 (defn- in-scope [{parent :parent names :names} name]
+  "Adds the 'name' given to the current scope of the
+  environment given."
   (environment. parent (conj names name)))
 
 (defn- nest [env names]
+  "Produces a nested environment (child of 'env') that has access
+  to the given list of 'names'"
   (environment. env (set names)))
 
 (defn- pop-env [env]
+  "Opposite of 'nest' function. Returns the parent environment of
+  the environment given. Calling this function on the root environment
+  is an error."
   {:pre [(some? (:parent env))]}
 
   (:parent env))
 
 (defn- resolved? [env name]
+  "Performs name resolution. Returns whether the given 'name' is
+  bound with respect to the environment given."
   {:pre [(some? env) (set? (:names env))]}
 
   (cond
@@ -54,24 +80,45 @@
     (some? (:parent env)) (resolved? (:parent env) name)
     :else false))
 
+;; ================================================================ ;;
+;;                         SCOPE VISITOR                            ;;
+;; ================================================================ ;;
+
+;; Implements the ast/visitor protocol in order to determine if every variable
+;; and function application is resolved. Traverses the entire AST maintaining
+;; an environment that contains all known symbols.
 (defrecord scope-visitor [environment])
 
 (defn- scoped [name {env :environment}]
+  "Adds the 'name' given to the list of known symbols of the visitor.
+  Useful after evaluating a function definition."
   (let [new-env (in-scope env name)]
     (scope-visitor. new-env)))
 
 (defn- nest-with [names {env :environment}]
+  "Produces a scope-visitor with a nested environment, including
+  the given list of 'names' to the known symbols."
   (let [nested-env (nest env names)]
     (scope-visitor. nested-env)))
 
 (defn- unscope [{env :environment}]
+  "Produces a scope-visitor that operates under the parent environment.
+  Just like 'pop-env', it is an error to call this function if the visitor
+  is operating with a root environment."
   (let [new-env (pop-env env)]
     (scope-visitor. new-env)))
 
 (defn- accept-coll [coll v]
+  "Visits every node in a collection sequentially, using the resulting visitor
+  from one node on the next."
   (let [verify (fn [v n] (accept n v))]
     (reduce verify v coll)))
 
+;; important note: every visitor function *must* return a scope-visitor record.
+;; The returned visitor will be used to visit subsequent nodes in the AST. This is
+;; important because, as the visitor traverses the AST, new names symbols may be bound
+;; and they should be taken into account when validating variables and function
+;; applications.
 (extend-type scope-visitor
   ast/visitor
 
@@ -128,6 +175,9 @@
          (accept val))))
 
 (defn- reduce-defs [defs initial-visitor]
+  "Validates the scope of each definition; if it succeeds, adds the name
+  of the defined function to the scope, making sure subsequent definitons
+  may refer to it."
   (let [accept-and-scope (fn ([v n] (do
                                      (accept n v)
                                      (scoped (:name n) v)))
@@ -135,6 +185,9 @@
     (reduce accept-and-scope initial-visitor defs)))
 
 (defn perform [{defs :defs e :e :as program}]
+  "Performs scope validation on every function definition and on the
+  program's expression. Returns the unmodified program. Halts execution
+  if a symbol that cannot be resolved is found."
   (let [visitor (scope-visitor. (new-environment))]
     (->> visitor
          (reduce-defs defs)
--- src/foppl/utils.clj
+++ src/foppl/utils.clj
@@ -6,7 +6,13 @@
   (throw (ex-info message {:type type})))
 
 (defn ice [msg]
+  "Throws an exception that is reported to the end user as an
+  internal compiler error. Should only be used in situations
+  deemed 'unreachable' and irrecoverable"
   (runtime-error :type :ice :message msg))
 
 (defn foppl-error [msg]
+  "Throws an exception that is reported to the end user as an
+  invalid FOPPL program. This error should be used when validating
+  the semantics of programs given to the compiler."
   (runtime-error :type :invalid-program :message msg))
