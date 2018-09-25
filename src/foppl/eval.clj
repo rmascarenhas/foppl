e2665379a286085d491abb844363b18e0a686db2
Renato Costa <renatomc@cs.ubc.ca>
2018-09-24 22:22:11 -0700

    Replace use of 'eval' with map of available functions.

--- src/foppl/eval.clj
+++ src/foppl/eval.clj
@@ -8,14 +8,24 @@
   (:require [foppl.ast :as ast :refer [accept]])
   (:import [foppl.ast constant fn-application])
   (:require [foppl.formatter :as formatter])
-  (:import [foppl.formatter formatter-visitor]))
+  (:import [foppl.formatter formatter-visitor])
+  (:require [foppl.utils :as utils]))
+
+;; defines a map from function name -> function object for every function
+;; that can potentially be applied. In practice, see `valid-fn?` definition
+;; for details of when functions can actually be applied from the context
+;; of a FOPPL program
+(def ^:private builtins-registry
+  (merge (ns-map 'clojure.core) (ns-map 'anglican.runtime)))
 
 ;; do not allow the evaluation of these functions in a FOPPL context
 (def ^:private forbidden-core-functions
   #{'binding 'eval 'loop})
 
 (defn- valid-fn? [f]
-  (let [;; remove functions with upper case characters in the name (most
+  {:pre [(string? f)]}
+
+  (let [ ;; remove functions with upper case characters in the name (most
         ;; likely Java class names)
         no-java-classes (fn [f] (= (s/lower-case f) f))
 
@@ -30,82 +40,56 @@
 
     (valid? f)))
 
-(defn- eval-str [str]
-  {:pre [(string? str)]}
-  "Evaluates an expression (as a string) in a sandboxed environment.
-  Functions from clojure.core and anglican.runtime are available."
 
-  (let [ ;; name of the active namespace when calling this function
-        previous-ns *ns*
+;; this simple visitor extracts the underlying constant from AST nodes
+;; of type 'constant'. Useful when performing partial evaluation, where
+;; the actual value of a node needs to be retrieved.
+(defrecord raw-visitor [])
 
-        ;; create a temporary namespace for evaluating the expression
-        ;; given (as a string)
-        eval-ns-name (gensym "foppl-eval-ns")
-        eval-ns (create-ns eval-ns-name)
-
-        ;; makes sure the temporary namespace create has access to
-        ;; clojure core functions and anglican runtime functions
-        _ (binding [*ns* eval-ns] (refer-clojure) (refer 'anglican.runtime))]
-
-    (try
-      (in-ns eval-ns-name)
-      (eval (read-string str))
-      (finally
-        (in-ns (ns-name previous-ns))
-        (remove-ns eval-ns-name)))))
-
-;; the wrapping visitor makes sure that AST nodes holding constant list values
-;; are wrapped to (list ...) function calls. This makes list results generated
-;; by partial evaluation of expressions to be reusable in the evaluation of
-;; other expressions during the compilation pipeline.
-(defrecord wrap-visitor [])
-
-(extend-type wrap-visitor
+(extend-type raw-visitor
   ast/visitor
 
-  (visit-constant [_ {c :n :as constant}]
-    (if (or (list? c) (chunked-seq? c))
-      (ast/fn-application. 'list (map (fn [n] (ast/constant. n)) c))
-      constant))
+  (visit-constant [_ {c :n}]
+    c)
 
-  (visit-variable [_ var]
-    var)
+  (visit-variable [_ _]
+    (utils/ice "Variables have no raw value"))
 
-  (visit-literal-vector [_ literal-vector]
-    literal-vector)
+  (visit-literal-vector [_ _]
+    (utils/ice "Literal vectors have no raw value"))
 
-  (visit-literal-map [_ literal-map]
-    literal-map)
+  (visit-literal-map [_ _]
+    (utils/ice "Literal maps have no raw value"))
 
-  (visit-definition [_ definition]
-    definition)
+  (visit-definition [_ _]
+    (utils/ice "Definitions have no raw value"))
 
-  (visit-local-binding [_ local-binding]
-    local-binding)
+  (visit-local-binding [_ _]
+    (utils/ice "Local bindings have no raw value"))
 
-  (visit-foreach [_ foreach]
-    foreach)
+  (visit-foreach [_ _]
+    (utils/ice "foreach have no raw values"))
 
-  (visit-loop [_ loop]
-    loop)
+  (visit-loop [_ _]
+    (utils/ice "Loops have no raw value"))
 
-  (visit-if-cond [_ if-cond]
-    if-cond)
+  (visit-if-cond [_ _]
+    (utils/ice "if expressions have no raw values"))
 
-  (visit-fn-application [_ fn-application]
-    fn-application)
+  (visit-fn-application [_ _]
+    (utils/ice "function applications have no raw values"))
 
-  (visit-sample [_ sample]
-    sample)
+  (visit-sample [_ _]
+    (utils/ice "Samples have no raw values"))
 
-  (visit-observe [_ observe]
-    observe)
+  (visit-observe [_ _]
+    (utils/ice "Observations have no raw values"))
   )
 
-(defn- wrap-coll [coll]
-  (let [visitor (wrap-visitor.)
-        wrap (fn [n] (accept n visitor))]
-    (map wrap coll)))
+(defn- raw-constants [coll]
+  (let [visitor (raw-visitor.)
+        raw(fn [n] (accept n visitor))]
+    (map raw coll)))
 
 (defrecord eval-visitor [])
 
@@ -149,26 +133,16 @@
   (visit-fn-application [v {name :name args :args :as fn-application}]
     (let [valid? (valid-fn? (str name))
 
-          ;; apart from being valid, a function definition needs to be resolvable
-          ;; in the sandboxed namespace where evaluation happens.
-          resolved? (and valid? (eval-str (str "(resolve '" name ")")))
-
-          ;; wrap arguments in (list ...) function calls. This makes sure that
-          ;; lists generated from previous partial evaluations can be used
-          ;; to generate further results
-          wrapped-args (wrap-coll args)
-          wrapped-fn (ast/fn-application. name wrapped-args)
-
-          ;; serialize the function being evaluated to a string so that
-          ;; it can be evaluated by Clojure
-          formatter (formatter/formatter-visitor.)
-          sexp (accept wrapped-fn formatter)]
+          ;; apart from being valid, a function definition needs to be resolvable --
+          ;; i.e., exist in the builtins registry
+          builtin (get builtins-registry name)
+          resolved? (and valid? builtin)]
 
       ;; if the function is resolved, return an AST node for a constant
       ;; holding the result of the evaluation. Otherwise, leave the
       ;; expression unchanged
       (if resolved?
-        (ast/constant. (eval-str sexp))
+        (ast/constant. (apply builtin (raw-constants args)))
         fn-application)))
 
   (visit-sample [_ sample]
@@ -179,8 +153,9 @@
   )
 
 (def ^:const all-functions
-  (let [sandbox-map (eval-str "(ns-map (ns-name *ns*))")
-        fns (keys sandbox-map)
+  "Set of all language builtins. Includes every function defined in clojure.core
+  and anglican.runtime (plus extra definitions for martrix operations)."
+  (let [fns (keys builtins-registry)
         valid-fns (map symbol (filter valid-fn? (map str fns)))]
     (set valid-fns)))
 
